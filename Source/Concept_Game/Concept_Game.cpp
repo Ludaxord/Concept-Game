6ff4e5a18006d119ed0b264b365787958f5f9bf2
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-09-26 22:44:50 +0200

    initial commit

---
+++ Source/Concept_Game/Concept_Game.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Concept_Game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Concept_Game, "Concept_Game" );
