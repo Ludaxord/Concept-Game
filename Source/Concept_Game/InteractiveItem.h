f131db522007d5a1d32d83f6117fef0befd4c8f3
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-10-02 23:59:48 +0200

    add enemy and interactive items

---
+++ Source/Concept_Game/InteractiveItem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "InteractiveItem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONCEPT_GAME_API AInteractiveItem : public AItem
+{
+	GENERATED_BODY()
+	
+};
