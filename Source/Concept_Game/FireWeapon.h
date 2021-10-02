3da2638cf4b726b2d760ad4961e80ea7c7494cfd
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-10-02 23:35:11 +0200

    add classes for weapons

---
+++ Source/Concept_Game/FireWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Weapon.h"
+#include "FireWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONCEPT_GAME_API AFireWeapon : public AWeapon
+{
+	GENERATED_BODY()
+	
+};
