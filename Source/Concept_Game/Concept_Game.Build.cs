58a85d6511d6f99d5f44ea086cf86b8c91fd9686
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-10-02 20:59:15 +0200

    new module

--- Source/Concept_Game/Concept_Game.Build.cs
+++ Source/Concept_Game/Concept_Game.Build.cs
@@ -9,7 +9,7 @@ public class Concept_Game : ModuleRules
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
         PublicDependencyModuleNames.AddRange(new string[]
-            { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "PhysicsCore", "NavigationSystem", "AIModule" });
+            { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "PhysicsCore", "NavigationSystem", "AIModule", "Paper2D" });
 
         PrivateDependencyModuleNames.AddRange(new string[] { });
 
