556c6df06cde650554aa7ae1059857d42638a78f
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-10-04 23:23:58 +0200

    add movement

--- Source/Concept_Game/MainCharacter.cpp
+++ Source/Concept_Game/MainCharacter.cpp
@@ -7,7 +7,18 @@
 #include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
-AMainCharacter::AMainCharacter() {
+AMainCharacter::AMainCharacter():
+	BaseTurnRate(45.0f),
+	BaseLookUpRate(45.0f),
+	bAiming(false),
+	HipTurnRate(90.0f),
+	HipLookUpRate(90.0f),
+	AimingTurnRate(20.0f),
+	AimingLookUpRate(20.0f),
+	MouseHipTurnRate(1.0f),
+	MouseHipLookUpRate(1.0f),
+	MouseAimingTurnRate(0.6f),
+	MouseAimingLookUpRate(0.6f) {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	bUseControllerRotationYaw = true;
@@ -36,5 +47,47 @@ void AMainCharacter::Tick(float DeltaTime) {
 // Called to bind functionality to input
 void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	check(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMainCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AMainCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("TurnRate", this, &AMainCharacter::TurnRate);
+	PlayerInputComponent->BindAxis("LookupRate", this, &AMainCharacter::LookUpAtRate);
+	PlayerInputComponent->BindAxis("Turn", this, &AMainCharacter::Turn);
+	PlayerInputComponent->BindAxis("LookUp", this, &AMainCharacter::LookUp);
+}
 
+void AMainCharacter::MoveForward(float Value) {
+	if (Controller != nullptr && (Value != 0.0f)) {
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation = {0, Rotation.Yaw, 0};
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMainCharacter::MoveRight(float Value) {
+	if (Controller != nullptr && (Value != 0.0f)) {
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation = {0, Rotation.Yaw, 0};
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void AMainCharacter::TurnRate(float Rate) {
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AMainCharacter::Turn(float Value) {
+	float TurnScaleFactor = bAiming ? MouseAimingTurnRate : MouseHipTurnRate;
+	AddControllerYawInput(Value * TurnScaleFactor);
+}
+
+void AMainCharacter::LookUpAtRate(float Rate) {
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AMainCharacter::LookUp(float Value) {
+	float LookUpScaleFactor = bAiming ? MouseAimingLookUpRate : MouseHipLookUpRate;
+	AddControllerPitchInput(Value * LookUpScaleFactor);
 }
--- Source/Concept_Game/MainCharacter.h
+++ Source/Concept_Game/MainCharacter.h
@@ -46,10 +46,51 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	void TurnRate(float Rate);
+	void Turn(float Value);
+	void LookUpAtRate(float Rate);
+	void LookUp(float Value);
+
 private:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Camera",meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Camera", meta = (AllowPrivateAccess = "true"))
 	class USpringArmComponent* CameraBoom;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Camera",meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Camera", meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* FollowCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	float BaseTurnRate;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	float BaseLookUpRate;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	float HipTurnRate;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	float HipLookUpRate;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	float AimingTurnRate;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	float AimingLookUpRate;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"),
+		meta = (ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"))
+	float MouseHipTurnRate;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"),
+		meta = (ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"))
+	float MouseHipLookUpRate;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"),
+		meta = (ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"))
+	float MouseAimingTurnRate;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"),
+		meta = (ClampMin = "0.0", ClampMax = "1.0", UIMin = "0.0", UIMax = "1.0"))
+	float MouseAimingLookUpRate;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+	bool bAiming;
 };
