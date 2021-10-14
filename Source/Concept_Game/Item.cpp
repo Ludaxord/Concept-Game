0444361a5ea87c0cdf95adbefb169717d6e13788
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-10-15 00:54:47 +0200

    preapre item

--- Source/Concept_Game/Item.cpp
+++ Source/Concept_Game/Item.cpp
@@ -18,6 +18,60 @@ void AItem::BeginPlay()
 	
 }
 
+void AItem::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) {
+}
+
+void AItem::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex) {
+}
+
+void AItem::SetItemProperties(EItemState State) {
+}
+
+void AItem::FinishInterp() {
+}
+
+void AItem::ItemInterp(float DeltaTime) {
+}
+
+FVector AItem::GetInterpLocation() {
+}
+
+void AItem::PlayPickupSound(bool bForcePlaySound) {
+}
+
+void AItem::InitializeCustomDepth() {
+}
+
+void AItem::OnConstruction(const FTransform& Transform) {
+	Super::OnConstruction(Transform);
+}
+
+void AItem::PlayEquipSound(bool bForcePlaySound) {
+}
+
+void AItem::EnableCustomDepth() {
+}
+
+void AItem::DisableCustomDepth() {
+}
+
+void AItem::EnableGlowMaterial() {
+}
+
+void AItem::DisableGlowMaterial() {
+}
+
+void AItem::UpdatePulse() {
+}
+
+void AItem::ResetPulseTimer() {
+}
+
+void AItem::StartPulseTimer() {
+}
+
 // Called every frame
 void AItem::Tick(float DeltaTime)
 {
