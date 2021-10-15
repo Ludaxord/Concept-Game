04ab810cd873c7a22d73a43721968881c1716a80
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-10-15 09:53:33 +0200

    create effects classes

---
+++ Source/Concept_Game/CustomDepth.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Effect.h"
+
+/**
+ * 
+ */
+class CONCEPT_GAME_API CustomDepth : public Effect {
+public:
+	CustomDepth();
+	~CustomDepth();
+};
