8de797e91b30fe71539ed66f85aea8f6abcc92b8
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-10-17 01:02:24 +0200

    create blueprints

--- Source/Concept_Game/Magazine.cpp
+++ Source/Concept_Game/Magazine.cpp
@@ -3,7 +3,21 @@
 
 #include "Magazine.h"
 
+#include "Components/BoxComponent.h"
+#include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+
 AMagazine::AMagazine() {
+	AmmoMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("AmmoMesh"));
+	SetRootComponent(AmmoMesh);
+
+	GetCollisionBox()->SetupAttachment(GetRootComponent());
+	GetPickupWidget()->SetupAttachment(GetRootComponent());
+	GetAreaSphere()->SetupAttachment(GetRootComponent());
+
+	AmmoCollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("AmmoCollisionSphere"));
+	AmmoCollisionSphere->SetupAttachment(GetRootComponent());
+	AmmoCollisionSphere->SetSphereRadius(50.0f);
 }
 
 void AMagazine::BeginPlay() {
