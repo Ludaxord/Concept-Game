a34ed069b85dc7cee31c22bd55e792c3d7a19235
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2021-10-22 21:59:06 +0200

    create movable box class and bp

---
+++ Source/Concept_Game/MovableBox.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MovableItem.h"
+#include "MovableBox.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONCEPT_GAME_API AMovableBox : public AMovableItem
+{
+	GENERATED_BODY()
+	
+};
