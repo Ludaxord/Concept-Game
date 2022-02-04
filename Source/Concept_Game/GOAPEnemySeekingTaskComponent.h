a2d2c8b1a78e3f827cb99af525ac69c89acbb8da
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2022-02-04 21:50:50 +0100

    create basic tasks for AI

---
+++ Source/Concept_Game/GOAPEnemySeekingTaskComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GOAPTaskComponent.h"
+#include "GOAPEnemySeekingTaskComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONCEPT_GAME_API UGOAPEnemySeekingTaskComponent : public UGOAPTaskComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Concept_Game/GOAPPatrollingTaskComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GOAPTaskComponent.h"
+#include "GOAPPatrollingTaskComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONCEPT_GAME_API UGOAPPatrollingTaskComponent : public UGOAPTaskComponent
+{
+	GENERATED_BODY()
+	
+};
