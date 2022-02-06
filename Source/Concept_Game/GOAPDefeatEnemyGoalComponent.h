4a64cd183d5569d32667b14116fc13c92b81bf72
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2022-02-06 01:28:58 +0100

    created goals

---
+++ Source/Concept_Game/GOAPDefeatEnemyGoalComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GOAPGoalComponent.h"
+#include "GOAPDefeatEnemyGoalComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONCEPT_GAME_API UGOAPDefeatEnemyGoalComponent : public UGOAPGoalComponent
+{
+	GENERATED_BODY()
+	
+};
