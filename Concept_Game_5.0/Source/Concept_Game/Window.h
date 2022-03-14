1e518521e662c753d01daf710370399d78756253
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2022-03-14 21:51:40 +0100

    add window class

---
+++ Concept_Game_5.0/Source/Concept_Game/Window.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractiveItem.h"
+#include "Window.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CONCEPT_GAME_API AWindow : public AInteractiveItem {
+	GENERATED_BODY()
+
+};
