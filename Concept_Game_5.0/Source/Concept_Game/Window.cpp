367f5ca70bd3a4011f2098f701eab81975b1680a
Konrad Uciechowski <konrad.uciechowski@gmail.com>
2022-03-15 20:41:22 +0100

    movev npc character

--- Concept_Game_5.0/Source/Concept_Game/Window.cpp
+++ Concept_Game_5.0/Source/Concept_Game/Window.cpp
@@ -3,3 +3,111 @@
 
 #include "Window.h"
 
+#include "Kismet/KismetMathLibrary.h"
+
+AWindow::AWindow(): bIsOpened(false) {
+	ItemInteractionName = "Open";
+	ItemName = "Window";
+
+	PhysicsBasedMesh->SetSimulatePhysics(false);
+	PhysicsBasedMesh->SetCollisionProfileName(FName("BlockAllDynamic"));
+
+	WindowMovementTransitionTimeline = CreateDefaultSubobject<UTimelineComponent>(
+		TEXT("WindowMovementTransitionTimeline"));
+}
+
+void AWindow::BeginPlay() {
+	Super::BeginPlay();
+
+	WindowMovementFunctionFloat.BindDynamic(this, &AWindow::UpdateWindowMovementTransitionTimeline);
+	if (WindowMovementTransitionFloatCurve) {
+		WindowMovementTransitionTimeline->AddInterpFloat(WindowMovementTransitionFloatCurve,
+		                                                 WindowMovementFunctionFloat);
+
+		CurrentRotYaw = PhysicsBasedMesh->GetComponentRotation().Yaw;
+		CurrentRotPitch = PhysicsBasedMesh->GetComponentRotation().Pitch;
+		CurrentRotRoll = PhysicsBasedMesh->GetComponentRotation().Roll;
+
+		CurrentLocX = PhysicsBasedMesh->GetComponentLocation().X;
+		CurrentLocY = PhysicsBasedMesh->GetComponentLocation().Y;
+		CurrentLocZ = PhysicsBasedMesh->GetComponentLocation().Z;
+
+		UE_LOG(LogTemp, Warning, TEXT("Setting Window Movement Transition"));
+	}
+}
+
+void AWindow::Tick(float DeltaSeconds) {
+	if (bIsOpenedRef != bIsOpened) {
+		float LerpRotYaw = UKismetMathLibrary::Lerp(bIsOpenedRef
+			                                            ? CurrentRotYaw
+			                                            : CurrentRotYaw - RotationYawAngle,
+		                                            bIsOpenedRef
+			                                            ? CurrentRotYaw - RotationYawAngle
+			                                            : CurrentRotYaw,
+		                                            CurrentWindowRotation);
+
+		float LerpRotPitch = UKismetMathLibrary::Lerp(bIsOpenedRef
+			                                              ? CurrentRotPitch
+			                                              : CurrentRotPitch - RotationPitchAngle,
+		                                              bIsOpenedRef
+			                                              ? CurrentRotPitch - RotationPitchAngle
+			                                              : CurrentRotPitch,
+		                                              CurrentWindowRotation);
+
+		float LerpRotRoll = UKismetMathLibrary::Lerp(bIsOpenedRef
+			                                             ? CurrentRotRoll
+			                                             : CurrentRotRoll - RotationRollAngle,
+		                                             bIsOpenedRef
+			                                             ? CurrentRotRoll - RotationRollAngle
+			                                             : CurrentRotRoll,
+		                                             CurrentWindowRotation);
+
+		float LerpLocX = UKismetMathLibrary::Lerp(bIsOpenedRef
+			                                          ? CurrentLocX
+			                                          : CurrentLocX - LocationXMovement,
+		                                          bIsOpenedRef
+			                                          ? CurrentLocX - LocationXMovement
+			                                          : CurrentLocX,
+		                                          CurrentWindowRotation);
+
+		float LerpLocY = UKismetMathLibrary::Lerp(bIsOpenedRef
+			                                          ? CurrentLocY
+			                                          : CurrentLocY - LocationYMovement,
+		                                          bIsOpenedRef
+			                                          ? CurrentLocY - LocationYMovement
+			                                          : CurrentLocY,
+		                                          CurrentWindowRotation);
+
+		float LerpLocZ = UKismetMathLibrary::Lerp(bIsOpenedRef
+			                                          ? CurrentLocZ
+			                                          : CurrentLocZ - LocationZMovement,
+		                                          bIsOpenedRef
+			                                          ? CurrentLocZ - LocationZMovement
+			                                          : CurrentLocZ,
+		                                          CurrentWindowRotation);
+
+		FRotator NewRot = {LerpRotPitch, LerpRotYaw, LerpRotRoll};
+		FVector NewLoc = {LerpLocX, LerpLocY, LerpLocZ};
+
+		SetWindowRotation(NewRot, NewLoc);
+	}
+}
+
+void AWindow::InteractWithItem(AMainCharacter* InCharacter) {
+	bIsOpenedRef = !bIsOpened;
+	WindowMovementTransitionTimeline->PlayFromStart();
+}
+
+void AWindow::SetWindowRotation(FRotator NewRotator, FVector NewLocation) {
+	PhysicsBasedMesh->SetWorldLocationAndRotation(NewLocation, NewRotator);
+	if (CurrentWindowRotation == 1.f) {
+		bIsOpened = !bIsOpened;
+		ItemInteractionName = bIsOpened ? "Close" : "Open";
+		CurrentWindowRotation = 0;
+	}
+}
+
+void AWindow::UpdateWindowMovementTransitionTimeline(float Output) {
+	UE_LOG(LogTemp, Warning, TEXT("Update Window Rot... %f"), Output);
+	CurrentWindowRotation = Output;
+}
--- Concept_Game_5.0/Source/Concept_Game/Window.h
+++ Concept_Game_5.0/Source/Concept_Game/Window.h
@@ -3,14 +3,79 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "InteractiveItem.h"
+#include "PhysicsBasedItem.h"
+#include "Components/TimelineComponent.h"
 #include "Window.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class CONCEPT_GAME_API AWindow : public AInteractiveItem {
+class CONCEPT_GAME_API AWindow : public APhysicsBasedItem {
 	GENERATED_BODY()
 
+public:
+	AWindow();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+	virtual void InteractWithItem(AMainCharacter* InCharacter) override;
+
+	virtual void SetWindowRotation(FRotator NewRotator, FVector NewLocation);
+private:
+	UFUNCTION()
+	void UpdateWindowMovementTransitionTimeline(float Output);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Window Properties", meta = (AllowPrivateAccess = "true"))
+	bool bIsOpened;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Window Properties", meta = (AllowPrivateAccess = "true"))
+	bool bIsOpenedRef;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Window Properties", meta = (AllowPrivateAccess = "true"))
+	FOnTimelineFloat WindowMovementFunctionFloat{};
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Window Properties", meta = (AllowPrivateAccess = "true"))
+	class UTimelineComponent* WindowMovementTransitionTimeline;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category= "Window Properties", meta = (AllowPrivateAccess = "true"))
+	float CurrentWindowRotation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Window Properties", meta = (AllowPrivateAccess = "true"))
+	float RotationYawAngle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Window Properties", meta = (AllowPrivateAccess = "true"))
+	float RotationPitchAngle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Window Properties", meta = (AllowPrivateAccess = "true"))
+	float RotationRollAngle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Window Properties", meta = (AllowPrivateAccess = "true"))
+	float LocationXMovement;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Window Properties", meta = (AllowPrivateAccess = "true"))
+	float LocationYMovement;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category= "Window Properties", meta = (AllowPrivateAccess = "true"))
+	float LocationZMovement;
+
+	UPROPERTY(EditAnywhere, Category="Window Properties", meta = (AllowPrivateAccess = "true"))
+	UCurveFloat* WindowMovementTransitionFloatCurve;
+
+	float CurrentRotYaw;
+
+	float CurrentRotPitch;
+
+	float CurrentRotRoll;
+
+	float CurrentLocX;
+
+	float CurrentLocY;
+
+	float CurrentLocZ;
+
+	FTimerHandle TimerHandle;
 };
